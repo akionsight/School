72938f81749e3febedd279665abff38186c5d004
AkIonSight <akionsight@gmail.com>
2022-09-24 20:51:51 +0530

    Update 20.py

--- 20.py
+++ 20.py
@@ -2,6 +2,6 @@ unprocessed_list = input("Enter a semicolon (;) seperated list: ")
 
 user_list = unprocessed_list.split(';')
 
-deduplicated_list = set(user_list) # A set cannot contain repeted 
+deduplicated_list = set(user_list) # Remember, elements in a set cannot contain repeated 
 
 print(list(deduplicated_list))
