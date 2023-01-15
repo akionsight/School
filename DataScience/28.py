cc1212ebea398f86b081ce2174b2be21c32aa44c
AkIonSight <akionsight@gmail.com>
2023-01-15 13:57:51 +0530

    arrays, but cooler

---
+++ DataScience/28.py
@@ -0,0 +1,9 @@
+import numpy as np 
+
+raw_data = input("Enter a semicolon seperated list of values (;)")
+
+values = raw_data.split(';')
+
+np_arr = np.array(values)
+
+print(f"The array is \n {np_arr}")
